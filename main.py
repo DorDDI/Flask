911fdaa8fa2931c340ec4192f7aada9c3df9b57c
DDI112 DDI112 <dorlasry71@gmail.com>
2023-04-20 21:16:57 +0300

    login manu add

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify, render_template,url_for
+from flask import Flask, request, jsonify, render_template, url_for,flash, redirect
+from forms import RegistrationForm, LoginForm
 import json
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e05201e52103ae9e0b478dc038802df6'
 
 posts = [
     {
@@ -18,27 +21,50 @@ posts = [
     }
 ]
 
+
 @app.route("/")
 def print():
     return "<h1>hello<h1>"
 
+
 @app.route("/home")
 def home():
-    return render_template('home.html', posts = posts,  title = 'Home')
+    return render_template('home.html', posts=posts, title='Home')
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template('about.html', title='About')
 
 
+@app.route("/register", methods=['Get', 'Post'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
+
+@app.route("/login", methods=['Get', 'Post'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You Logged In!!!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unseccessful. Please Try Again', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 # Define a sample route to return a list of items
-@app.route('/drinks',methods=['GET'])
+@app.route('/drinks', methods=['GET'])
 def get_items():
-    items = jsonify({"drinks": [{"description": "good 1", "name": "apple"},{"description": "good 2", "name": "orange"},{"description": "good 3", "name": "grapes"}]})
+    items = jsonify({"drinks": [{"description": "good 1", "name": "apple"}, {"description": "good 2", "name": "orange"},
+                                {"description": "good 3", "name": "grapes"}]})
     return items
+
 
 # Define a route to add a new item
 @app.route('/items', methods=['POST'])
@@ -46,7 +72,6 @@ def add_item():
     item = request.json['item']
     # Here you could add the new item to a database or some other data store
     return jsonify({'success': True})
-
 
 
 if __name__ == '__main__':
